ab9c49992055f572bd6905c8e01ceac125b0cc4b
bjiang-merkle <73517591+bjiang-merkle@users.noreply.github.com>
2022-10-11 10:56:11 +0800

    Add files via upload

---
+++ ecommerce.js
@@ -0,0 +1,41 @@
+function product_click(){
+datalayer.push({
+
+})
+}
+
+function product_view(){
+datalayer.push({
+   
+})
+}
+
+function add_to_shopping_cart(){
+datalayer.push({
+   
+})
+}
+
+function remove_from_shopping_cart(){
+datalayer.push({
+   
+})
+}
+
+function checkout_order_submit(){
+datalayer.push({
+  
+})
+}
+
+function payment_successful(){
+datalayer.push({
+   
+});
+}
+
+function refund_and_return_confirm(){
+datalayer.push({
+   
+});
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+function header_click(){
+    console.log("dddd")
+    datalayer.push({
+        // 'event':'GA-event',
+        // 'eventDetail':{
+        //     'category':'search',
+        //     'action':'黑色文胸',
+        //     'label':'undefined'
+        // }
+    })
+}
